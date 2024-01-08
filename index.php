fce469f8fd9a28133113ac29d80a80520a461be4
adawg2010 <jcebersole1@gmail.com>
2024-01-08 20:28:07 +0900

    footingandheading

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
-<?php 
-
+<?php
+/**
+ * The main template file.
+ */
+get_header();
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  
-</body>
-</html>
+<?php
+get_footer();
