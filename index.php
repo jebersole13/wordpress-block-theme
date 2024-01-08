b0ed35cae9e29d1b4ab6e9cca568bc4667660f4c
adawg2010 <jcebersole1@gmail.com>
2024-01-08 20:53:25 +0900

    index update

--- index.php
+++ index.php
@@ -3,7 +3,16 @@
  * The main template file.
  */
 get_header();
-?>
 
-<?php
+
+if (<?php if ( have_posts() ) : ?>
+
+  <?php while ( have_posts() ) : the_post(); ?>
+
+    <?php the_content(); ?>
+
+  <?php endwhile; ?>
+
+<?php endif; ?>
+<?php wp_reset_postdata(); ?>)
 get_footer();
