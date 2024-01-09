7805380a699abd922831554e37d87da95d72f00c
adawg2010 <jcebersole1@gmail.com>
2024-01-09 12:45:04 +0900

    content mod

--- template-parts/post/content.php
+++ template-parts/post/content.php
@@ -5,9 +5,18 @@
 <article id="<?php the_ID()?>">
 <header class="entry-header">
   <?php 
-  the_title()
+  the_title('<h2 class="entry-title"><a class="entry-link" href="'.esc_url( get_permalink() ).'">', '</a></h2>' );
   ?>
 
 </header>
+<?php 
+if(has_post_thumbnail()):
+  the_post_thumbnail('large');
 
+  endif;
+?>
+
+<div class="entry-content">
+  <?php the_excerpt(); ?>
+</div>
 </article>
