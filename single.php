69f6c4327da1e0bbee7615d6116392aa99247fad
adawg2010 <jcebersole1@gmail.com>
2024-01-09 12:58:33 +0900

    single commit

--- single.php
+++ single.php
@@ -1,1 +1,32 @@
 <?php 
+
+/**
+
+*/
+
+get_header();
+?>
+<div id="primary" class=" content-area">
+  
+<main id="main" class="site-main">
+
+
+<?php if ( have_posts() ) : 
+
+ while ( have_posts() ) : 
+    the_post(); 
+
+    get_template_part('template-parts/post/content');
+ endwhile; 
+
+if(comments_open() or get_comments_number()) :
+  comments_template(); 
+  ?>
+
+
+</main>
+
+</div>
+
+<?php 
+get_footer();
